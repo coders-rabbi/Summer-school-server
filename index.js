88858be9e03a3f246498b278ee737db06c9f9fd8
coders rabbi <pcrabbi2020@gmail.com>
2023-06-13 01:09:15 +0600

    jwt error solve

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, env.process.ACCESS_TOKEN, { expiresIn: '1h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
             res.send(token);
         })
 
